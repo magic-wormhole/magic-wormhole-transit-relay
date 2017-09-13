46abd75fda8c61c359fd1aa3cc31444b41b3d8b8
Brian Warner <warner@lothar.com>
2017-09-12 23:46:56 -0700

    move transit-relevant files out from magic-wormhole
    These files are copied (with roughly-appropriate changes to the top-level
    setup.py, NEWS.md, etc) from magic-wormhole 0.10.3, commit
    be166b483c5796ab3a9ad588ccf671b7eabdd96c).

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+User-visible changes in "magic-wormhole-transit-relay":
+
+
+
+## forked from magic-wormhole-0.10.3 (12-Sep-2017)
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+import versioneer
+
+commands = versioneer.get_cmdclass()
+
+setup(name="magic-wormhole-transit-relay",
+      version=versioneer.get_version(),
+      description="Transit Relay server for Magic-Wormhole",
+      author="Brian Warner",
+      author_email="warner-magic-wormhole@lothar.com",
+      license="MIT",
+      url="https://github.com/warner/magic-wormhole-transit-relay",
+      package_dir={"": "src"},
+      packages=["wormhole_transit_relay",
+                "wormhole_transit_relay.test",
+                ],
+      package_data={"wormhole_transit_relay": ["db-schemas/*.sql"]},
+      install_requires=[
+          "twisted >= 17.5.0",
+      ],
+      extras_require={
+          ':sys_platform=="win32"': ["pypiwin32"],
+          "dev": ["mock", "tox", "pyflakes"],
+      },
+      test_suite="wormhole_transit_relay.test",
+      cmdclass=commands,
+      )
