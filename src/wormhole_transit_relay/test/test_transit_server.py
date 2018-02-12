4e9b6c53a9a1d8cff0aab22d7451bd7dddf71ee5
Brian Warner <warner@lothar.com>
2018-02-12 13:35:29 -0800

    rework mood tracking, full tests of usage events

--- src/wormhole_transit_relay/test/test_transit_server.py
+++ src/wormhole_transit_relay/test/test_transit_server.py
@@ -214,6 +214,10 @@ class _Transit:
         ep = clientFromString(reactor, self.transit)
         a1 = yield connectProtocol(ep, Accumulator())
         a2 = yield connectProtocol(ep, Accumulator())
+        a3 = yield connectProtocol(ep, Accumulator())
+        disconnects = []
+        a1._disconnect.addCallback(disconnects.append)
+        a2._disconnect.addCallback(disconnects.append)
 
         token1 = b"\x00"*32
         side1 = b"\x01"*8
@@ -229,8 +233,26 @@ class _Transit:
             yield wait()
         self.assertEqual(self.count(), 2) # same-side connections don't match
 
+        # when the second side arrives, the spare first connection should be
+        # closed
+        side2 = b"\x02"*8
+        a3.transport.write(b"please relay " + hexlify(token1) +
+                           b" for side " + hexlify(side2) + b"\n")
+        # let that arrive
+        while self.count() != 0:
+            yield wait()
+        self.assertEqual(len(self._transit_server._pending_requests), 0)
+        self.assertEqual(len(self._transit_server._active_connections), 2)
+        # That will trigger a disconnect on exactly one of (a1 or a2). Wait
+        # until our client notices it.
+        while not disconnects:
+            yield wait()
+        # the other connection should still be connected
+        self.assertEqual(sum([int(t.transport.connected) for t in [a1, a2]]), 1)
+
         a1.transport.loseConnection()
         a2.transport.loseConnection()
+        a3.transport.loseConnection()
 
     @defer.inlineCallbacks
     def test_bad_handshake_old(self):
@@ -379,3 +401,143 @@ class TransitWithLogs(_Transit, ServerBase, unittest.TestCase):
 
 class TransitWithoutLogs(_Transit, ServerBase, unittest.TestCase):
     log_requests = False
+
+class Usage(ServerBase, unittest.TestCase):
+    @defer.inlineCallbacks
+    def setUp(self):
+        yield super(Usage, self).setUp()
+        self._usage = []
+        def record(started, result, total_bytes, total_time, waiting_time):
+            self._usage.append((started, result, total_bytes,
+                                total_time, waiting_time))
+        self._transit_server.recordUsage = record
+
+    @defer.inlineCallbacks
+    def test_errory(self):
+        ep = clientFromString(reactor, self.transit)
+        a1 = yield connectProtocol(ep, Accumulator())
+
+        a1.transport.write(b"this is a very bad handshake\n")
+        # that will log the "errory" usage event, then drop the connection
+        yield a1._disconnect
+        self.assertEqual(len(self._usage), 1, self._usage)
+        (started, result, total_bytes, total_time, waiting_time) = self._usage[0]
+        self.assertEqual(result, "errory", self._usage)
+
+    @defer.inlineCallbacks
+    def test_lonely(self):
+        ep = clientFromString(reactor, self.transit)
+        a1 = yield connectProtocol(ep, Accumulator())
+
+        token1 = b"\x00"*32
+        side1 = b"\x01"*8
+        a1.transport.write(b"please relay " + hexlify(token1) +
+                           b" for side " + hexlify(side1) + b"\n")
+        while not self._transit_server._pending_requests:
+            yield wait() # wait for the server to see the connection
+        # now we disconnect before the peer connects
+        a1.transport.loseConnection()
+        yield a1._disconnect
+        while self._transit_server._pending_requests:
+            yield wait() # wait for the server to see the disconnect too
+
+        self.assertEqual(len(self._usage), 1, self._usage)
+        (started, result, total_bytes, total_time, waiting_time) = self._usage[0]
+        self.assertEqual(result, "lonely", self._usage)
+        self.assertIdentical(waiting_time, None)
+
+    @defer.inlineCallbacks
+    def test_one_happy_one_jilted(self):
+        ep = clientFromString(reactor, self.transit)
+        a1 = yield connectProtocol(ep, Accumulator())
+        a2 = yield connectProtocol(ep, Accumulator())
+
+        token1 = b"\x00"*32
+        side1 = b"\x01"*8
+        side2 = b"\x02"*8
+        a1.transport.write(b"please relay " + hexlify(token1) +
+                           b" for side " + hexlify(side1) + b"\n")
+        while not self._transit_server._pending_requests:
+            yield wait() # make sure a1 connects first
+        a2.transport.write(b"please relay " + hexlify(token1) +
+                           b" for side " + hexlify(side2) + b"\n")
+        while not self._transit_server._active_connections:
+            yield wait() # wait for the server to see the connection
+        self.assertEqual(len(self._transit_server._pending_requests), 0)
+        self.assertEqual(self._usage, []) # no events yet
+        a1.transport.write(b"\x00" * 13)
+        yield a2.waitForBytes(13)
+        a2.transport.write(b"\xff" * 7)
+        yield a1.waitForBytes(7)
+
+        a1.transport.loseConnection()
+        yield a1._disconnect
+        while self._transit_server._active_connections:
+            yield wait()
+        yield a2._disconnect
+        self.assertEqual(len(self._usage), 1, self._usage)
+        (started, result, total_bytes, total_time, waiting_time) = self._usage[0]
+        self.assertEqual(result, "happy", self._usage)
+        self.assertEqual(total_bytes, 20)
+        self.assertNotIdentical(waiting_time, None)
+
+    @defer.inlineCallbacks
+    def test_redundant(self):
+        ep = clientFromString(reactor, self.transit)
+        a1a = yield connectProtocol(ep, Accumulator())
+        a1b = yield connectProtocol(ep, Accumulator())
+        a1c = yield connectProtocol(ep, Accumulator())
+        a2 = yield connectProtocol(ep, Accumulator())
+
+        token1 = b"\x00"*32
+        side1 = b"\x01"*8
+        side2 = b"\x02"*8
+        a1a.transport.write(b"please relay " + hexlify(token1) +
+                            b" for side " + hexlify(side1) + b"\n")
+        def count_requests():
+            return sum([len(v)
+                        for v in self._transit_server._pending_requests.values()])
+        while count_requests() < 1:
+            yield wait()
+        a1b.transport.write(b"please relay " + hexlify(token1) +
+                            b" for side " + hexlify(side1) + b"\n")
+        while count_requests() < 2:
+            yield wait()
+
+        # connect and disconnect a third client (for side1) to exercise the
+        # code that removes a pending connection without removing the entire
+        # token
+        a1c.transport.write(b"please relay " + hexlify(token1) +
+                            b" for side " + hexlify(side1) + b"\n")
+        while count_requests() < 3:
+            yield wait()
+        a1c.transport.loseConnection()
+        yield a1c._disconnect
+        while count_requests() > 2:
+            yield wait()
+        self.assertEqual(len(self._usage), 1, self._usage)
+        (started, result, total_bytes, total_time, waiting_time) = self._usage[0]
+        self.assertEqual(result, "lonely", self._usage)
+
+        a2.transport.write(b"please relay " + hexlify(token1) +
+                           b" for side " + hexlify(side2) + b"\n")
+        # this will claim one of (a1a, a1b), and close the other as redundant
+        while not self._transit_server._active_connections:
+            yield wait() # wait for the server to see the connection
+        self.assertEqual(count_requests(), 0)
+        self.assertEqual(len(self._usage), 2, self._usage)
+        (started, result, total_bytes, total_time, waiting_time) = self._usage[1]
+        self.assertEqual(result, "redundant", self._usage)
+
+        # one of the these is unecessary, but probably harmless
+        a1a.transport.loseConnection()
+        a1b.transport.loseConnection()
+        yield a1a._disconnect
+        yield a1b._disconnect
+        while self._transit_server._active_connections:
+            yield wait()
+        yield a2._disconnect
+        self.assertEqual(len(self._usage), 3, self._usage)
+        (started, result, total_bytes, total_time, waiting_time) = self._usage[2]
+        self.assertEqual(result, "happy", self._usage)
+
