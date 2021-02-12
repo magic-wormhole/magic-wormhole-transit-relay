4669619f7e18d4c49746331c90c4f8586adab86b
meejah <meejah@meejah.ca>
2021-02-12 01:09:16 -0700

    skip usage-counting if we're jilted but other side is happy?

--- src/wormhole_transit_relay/server_state.py
+++ src/wormhole_transit_relay/server_state.py
@@ -362,6 +362,10 @@ class TransitServerState(object):
     # some outputs to record "usage" information ..
     @_machine.output()
     def _record_usage(self):
+        if self._mood == "jilted":
+            if self._buddy:
+                if self._buddy._mood == "happy":
+                    return
         self._usage.record(
             started=self._client.started_time,
             buddy_started=self._buddy._client.started_time if self._buddy is not None else None,
